720957fad113e7cba17a5c85fde947b5886a03bb
Jake Cheek <jake@f-m.fm>
2023-12-13 14:48:29 -0500

    accounts page dark mode

--- src/components/AccountPage/index.tsx
+++ src/components/AccountPage/index.tsx
@@ -15,13 +15,13 @@ const AccountPage: FC<{ id: number }> = ({ id }) => {
       <div
         className={clsx(
           'h-full min-h-0 p-4',
-          'sm:bg-texture sm:flex sm:w-full sm:items-center sm:justify-center',
+          'sm:bg-texture sm:dark:bg-texture-dark sm:flex sm:w-full sm:items-center sm:justify-center',
         )}
       >
         <div
           className={clsx(
             'h-full',
-            'sm:h-[600px] sm:w-[500px] sm:border sm:bg-white sm:p-4 sm:shadow',
+            'rounded border-gray-200 dark:border-gray-700 dark:bg-gray-900 sm:h-[600px] sm:w-[500px] sm:border sm:bg-white sm:p-4 sm:shadow',
           )}
         >
           <HasData account={accountQuery.data} />
